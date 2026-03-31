98b8ac881cb83f579692a7ac04d3247179252941
Soon-1234 <sosolkjeon21@gmail.com>
2026-03-31 11:54:54 +0300

    doublon

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -28,7 +28,11 @@
             <?php endif; ?>
             <nav>
                 <a href="/achat">Saisie Achat</a>
+                <a href="#">Menu 1</a>
+                <a href="#">Menu 2</a>
+                <a href="#">Menu 3</a>
             </nav>
+
         </aside>
 
         <main>
