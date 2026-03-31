93432620a9d8f745908365918c8028fe423f4aa0
Soon-1234 <sosolkjeon21@gmail.com>
2026-03-31 10:58:01 +0300

    index racine

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+require 'config/database.php';
+
+
+Flight::route('GET /', function() {
+    $controller = new app\controllers\CaisseController();
+    $controller->index();
+});
+
+Flight::route('POST /caisse/choisir', function() {
+    $controller = new app\controllers\CaisseController();
+    $controller->choisir();
+});
+
+
+
+Flight::start();
