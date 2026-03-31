8d5e237d812a9238463ae9cd693afaba946e0a98
Soon-1234 <sosolkjeon21@gmail.com>
2026-03-31 10:34:01 +0300

    header-footer (misaraka amin'ny body )

---
+++ app/views/layouts/main.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/shop-homepage.css">
+    <title>Shop Ma</title>
+</head>
+
+<body>
+    <header> TD - SI-IHM - ETU0004159-ETU0004274</header>
+    <div class="body">
+        <aside cla>
+         <h2> Caisse n°1</h2>
+         <ul>
+            <a #href>Menu 1</a>
+            <a #href>Menu 2</a>
+            <a #href>Menu 3</a>
+         </ul>
+        </aside>
+
+        <main>
+            <?php require $view; ?>
+        </main>
+
+    </div>
+
+     <footer>
+        Copyright &copy; 4159-4274 Shop Ma 
+    </footer>
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
